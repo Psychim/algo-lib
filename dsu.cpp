692261af05557190d70b0f7ee2ce21ffd5078056
Psychim <604315924@qq.com>
2018-09-27 03:41:11 +0800

    update dsu

--- dsu.cpp
+++ dsu.cpp
@@ -2,6 +2,11 @@
 using namespace std;
 const int maxn=1e5;
 int father[maxn];
+void initdsu(int n){
+    for(int i=0;i<n;i++){
+        father[i]=i;
+    }
+}
 int findset(int u){
     if(father[u]==u)return u;
     return father[u]=findset(father[u]);
